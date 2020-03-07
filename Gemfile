a87c4054d891b331d333bc610cdbe48bf99500df
AjxLab <abe12@mccc.jp>
2020-03-07 11:18:36 +0900

    add eventmachine

--- Gemfile
+++ Gemfile
@@ -8,5 +8,7 @@ gem "rake", "~> 12.0"
 gem "rspec", "~> 3.0"
 
 gem "husc", ">= 0.2.2"
+gem "eventmachine"
+gem "em-http-request", require: "em-http"
 gem "ruby-gmail", require: "gmail"
 
--- spider.rb
+++ spider.rb
@@ -4,6 +4,36 @@ require './mail'
 
 
 START_TIME = Time.new
+URL = 'http://www.example.com/'
+
+
+def crawl(delay: 3, depth_limit: nil, multi: false)
+  ## -----*----- クローリング -----*----- ##
+  # delay：Float --> アクセス間隔
+  # depth_limit：Int --> クロールする深さ
+  # multi：Bool --> 並列処理(true) or 逐次処理(false)
+
+  # ターゲットURLを指定（Array）
+  urls = [URL]
+
+  if multi
+    # 並列処理 ==================================
+    pending = urls.length
+    #tasks = urls.map { |url|
+    #  {
+    #    # 取得ステータス（0：未完了, 1：完了）
+    #    status: 0,
+    #    # データを取得する処理
+    #    task: Proc.new { Husc.new(url) },
+    #    #
+    #  }
+    }
+  else
+    # 逐次処理 ==================================
+  end
+
+  agent = Husc.new(URL)
+end
 
 
 def footer_exit(status: 0)
@@ -25,6 +55,7 @@ end
 
 
 if __FILE__ == $0
+  crawl
   footer_exit
 end
 
