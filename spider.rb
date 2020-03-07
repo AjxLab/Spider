8374712eaf9662a6b81548dfd001d4e38b541c5a
AjxLab <abe12@mccc.jp>
2020-03-07 12:18:00 +0900

    exit_all

--- spider.rb
+++ spider.rb
@@ -8,14 +8,14 @@ URL = 'http://www.example.com/'
 LOG = Logger.new('develop.log')
 
 
-def crawl(delay: 3, depth_limit: nil, multi: false)
+def crawl(delay: 3, depth_limit: nil, multi: false, exit_all: true)
   ## -----*----- クローリング -----*----- ##
   # delay：Float --> アクセス間隔
   # depth_limit：Int --> クロールする深さ
   # multi：Bool --> 並列処理(true) or 逐次処理(false)
 
   # ターゲットURLを指定（Array）
-  urls = [URL, URL, URL, 'http://1028051']
+  urls = [URL, URL, URL, 'http://1028051', URL]
 
   task = Proc.new { |url|
     begin
@@ -26,6 +26,7 @@ def crawl(delay: 3, depth_limit: nil, multi: false)
       # ==============
     rescue => e
       LOG.error("#{e}")
+      footer_exit status: 1  if exit_all
     end
   }
 
@@ -67,7 +68,7 @@ end
 
 
 if __FILE__ == $0
-  crawl(multi: false)
+  crawl(multi: true)
   footer_exit
 end
 
